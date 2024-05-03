1dbaf85ff454f91d07245b39af98d28e91504270
AsenKimO <148381508+AsenKimO@users.noreply.github.com>
2024-05-03 00:31:59 -0400

    snapkit dependency installed

--- SkillHub/SkillHub/Controllers/HomePageViewController.swift
+++ SkillHub/SkillHub/Controllers/HomePageViewController.swift
@@ -6,15 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomePageViewController: UIViewController {
 
+    // MARK: - Properties (view)
+    private var featuredUserView = UIView()
+    private var collectionView = UICollectionView()
+    
+    // MARK: - Properties (data)
+    private var users: [User] = []
+    private var filteredUsers: [User] = []
+    private var filters = ["All"]
+    private var currFilter = 0
+    
+    // MARK: - View Cycles
+    private let refreshControl = UIRefreshControl()
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupFeaturedUser()
     }
 
-    private func setupFeatured(){}
+    private func setupFeaturedUser(){
+        
+    }
 
 }
 
